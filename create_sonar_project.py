6504636cc4e6565d16bc5157e60e60501d4c0967
deese <dsargisyan@gmail.com>
2022-07-30 21:53:19 +0400

    first commit

---
+++ create_sonar_project.py
@@ -0,0 +1,22 @@
+from github import Github
+
+import os
+
+github_acc_token = os.getenv('GH_ACC_T')
+github_repo_name = os.getenv('GH_R_N')
+
+secret_var = os.getenv("VAR")
+
+gh = Github(github_acc_token)
+print('github initialized')
+
+repo = gh.get_repo(github_repo_name, lazy=False)
+
+if not secret_var:
+    print('Do something')
+    repo.create_secret(secret_name="VAR", unencrypted_value='true')
+    print('Secret updated')
+else:
+    print('Nothing to do')
+
+
